db1b3aeefb2013b089e53d0d6a6cdc130d395f58
odev <ts8irolon@gmail.com>
2022-08-05 12:23:51 -0300

    Move <Notifier> in the new dir. Where consume rick&mortyAPI

---
+++ src/requests/Notifier.js
@@ -0,0 +1,11 @@
+/*getAllLocations */
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+class Notifier {
+  static async getAllByEntity(entity) {
+    const response = await fetch(`https://rickandmortyapi.com/api/${entity}`);
+    return await response.json();
+  }
+}
+
+module.exports = Notifier;
