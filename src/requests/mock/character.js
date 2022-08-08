ddba2eadd503cdd68c0a678f609b46c3e9236f62
odev <ts8irolon@gmail.com>
2022-08-08 12:59:05 -0300

    Implement new data in the mock

--- src/requests/mock/character.js
+++ src/requests/mock/character.js
@@ -17,13 +17,29 @@ const data = {
       url: 'https://rickandmortyapi.com/api/character/8',
       created: '2017-11-04T20:03:34.737Z',
     },
+    {
+      id: 8,
+      name: 'Adjudicator Rick',
+      status: 'Dead',
+      species: 'Human',
+      type: '',
+      gender: 'Male',
+      origin: { name: 'unknown', url: '' },
+      location: {
+        name: 'Citadel of Ricks',
+        url: 'https://rickandmortyapi.com/api/location/3',
+      },
+      image: 'https://rickandmortyapi.com/api/character/avatar/8.jpeg',
+      episode: ['https://rickandmortyapi.com/api/episode/28'],
+      url: 'https://rickandmortyapi.com/api/character/8',
+      created: '2017-11-04T20:03:34.737Z',
+    },
   ],
 };
 
-const standardChar = 2;
-
 const char = 'c';
 
+const repetitions = 4;
 const entity = 'character';
 
-module.exports = { data, char, standardChar, entity };
+module.exports = { data, char, repetitions, entity };
