0c98380d219325cc18cb4a27adefe040d2af94c2
odev <ts8irolon@gmail.com>
2022-08-08 07:37:47 -0300

    Implement test return skeleton the counter

---
+++ test/suppliers/counter.test.js
@@ -0,0 +1,16 @@
+const counter = require('../../src/suppliers/counter');
+const mock = require('../../src/requests/mock/character');
+
+describe('Counter reaction in the skeleton return: ', () => {
+  const skeleton = {
+    char: mock.char,
+    count: mock.standardChar,
+    resource: mock.entity,
+  };
+
+  test('should return basic skeleton <char, count, resource> not undefined or null', async () => {
+    const result = await counter({ char: mock.char, entity: mock.entity });
+
+    expect(result).toStrictEqual(skeleton);
+  });
+});
