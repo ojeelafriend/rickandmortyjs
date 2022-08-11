2398c5589c05cf4fbf7e530449d9ef9907d885da
odev <ts8irolon@gmail.com>
2022-08-11 11:25:13 -0300

    Abstraction of initial info, each setting method

--- src/suppliers/packer.js
+++ src/suppliers/packer.js
@@ -1,19 +1,22 @@
+const buildInitialInfo = {
+  excercise_name: '',
+  time: 0,
+  in_time: false,
+  results: undefined,
+};
+
 const buildCounter = (location, episode, character) => {
-  return {
-    excercise_name: 'Char counter',
-    time: 0,
-    in_time: false,
-    results: [location, episode, character],
-  };
+  buildInitialInfo.excercise_name = 'Char counter';
+  buildInitialInfo.results = [location, episode, character];
+
+  return buildInitialInfo;
 };
 
 const buildEpisodes = (episodeWithLocations) => {
-  return {
-    excercise_name: 'Episode locations',
-    time: 0,
-    in_time: false,
-    results: episodeWithLocations,
-  };
+  buildInitialInfo.excercise_name = 'Episode locations';
+  buildInitialInfo.results = episodeWithLocations;
+
+  return buildInitialInfo;
 };
 
 module.exports = { buildCounter, buildEpisodes };
