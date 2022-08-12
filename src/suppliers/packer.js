8c85fa806ad002313ec8536897018b6b8d652918
ojeelafriend <63030605+ojeelafriend@users.noreply.github.com>
2022-08-12 10:35:55 -0300

    Hotfix (#6)
    * Clean return [array]
    
    * Clear mocks
    
    * Change grammatic error: exercise

--- src/suppliers/packer.js
+++ src/suppliers/packer.js
@@ -1,6 +1,5 @@
-
 const buildInitialInfo = {
-  excercise_name: '',
+  exercise_name: '',
   time: 0,
   in_time: false,
   results: undefined,
@@ -13,18 +12,17 @@ const buildInitialInfo = {
 */
 
 const buildCounter = (location, episode, character) => {
-  buildInitialInfo.excercise_name = 'Char counter';
+  buildInitialInfo.exercise_name = 'Char counter';
   buildInitialInfo.results = [location, episode, character];
 
   return buildInitialInfo;
 };
 
 const buildEpisodes = (episodeWithLocations) => {
-  buildInitialInfo.excercise_name = 'Episode locations';
+  buildInitialInfo.exercise_name = 'Episode locations';
   buildInitialInfo.results = episodeWithLocations;
 
   return buildInitialInfo;
 };
 
 module.exports = { buildCounter, buildEpisodes };
-
--- test/dispatcher.test.js
+++ test/dispatcher.test.js
@@ -1,16 +1,14 @@
-
 const { counterSkeleton, episodesWithLocationsSkeleton, initialInfo } = require('../src/requests/mock/outputs');
 
 const dispatcher = require('../src/dispatcher');
 
 describe('Dispatch counter', () => {
   test('should return integration the suppliers (counter with packer build)', async () => {
-
     const { location, episode, character } = counterSkeleton;
 
     const result = await dispatcher.dispatchCounter();
 
-    initialInfo.excercise_name = 'Char counter';
+    initialInfo.exercise_name = 'Char counter';
     initialInfo.time = result.time;
     initialInfo.in_time = result.in_time;
 
@@ -24,13 +22,12 @@ describe('Dispatch episode locations', () => {
   test('should return integration the suppliers (packer build)', async () => {
     const result = await dispatcher.dispatchEpisode();
 
-    initialInfo.excercise_name = 'Episode locations';
+    initialInfo.exercise_name = 'Episode locations';
     initialInfo.time = result.time;
     initialInfo.in_time = result.in_time;
 
     initialInfo.results = episodesWithLocationsSkeleton;
 
     expect(result).toStrictEqual(initialInfo);
-
   });
 });
